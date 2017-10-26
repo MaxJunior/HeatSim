49b7ee97298a868fe955e7767590ee8b70b3cb1d
lp24 <lp2498@gmail.com>
2017-10-26 13:45:28 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 /*
-// Projeto SO - exercicio 1
+// Projeto SO - exercicio 2
 // Sistemas Operativos, DEI/IST/ULisboa 2017-18
 
-Luís Pedro Olivera Ferreira, nº83500
+Grupo 61
+
+Aluno : Luís Pedro Olivera Ferreira, num:83500
+Aluno : Maxwell SMart Ntido Junior , num:79457
 */
 
 #include <stdio.h>
@@ -11,23 +14,25 @@ Luís Pedro Olivera Ferreira, nº83500
 #include <pthread.h>
 
 #include "matrix2d.h"
-#include "mplib3.h"
 
+pthread_mutex_t   mutex;
+pthread_mutex_t   max_mutex;
+pthread_cond_t    espera_por_todos;
+pthread_cond_t    wait_to_wait;
 
+double max =0;
+int tem_esperar=0;
+int saiu=0;
+DoubleMatrix2D *matrix,*matrix_aux,*tmp;
 
 typedef struct{
-
+    double maxD;
     int n_iter;
-    int t_id;
+    int first_line;
+    int last_line;
     int colunas;
     int n_tarefas;
-    double dir;
-    double esq;
-    double baixo;
-    double cima;
     }info_t;
-    
-    
     
 /*--------------------------------------------------------------------
 | Function: simul
@@ -35,84 +40,115 @@ typedef struct{
 
 void *simul(void *information) {
 
-    //inicializacao das variaveis e criacao das matrizes
+    //inicializacao das variaveis
     info_t *slave_info=(info_t *)information;
-    DoubleMatrix2D *matrix, *matrix_aux, *tmp;
-    int k,i,j;
-    double value;
-    int id=slave_info->t_id;
+    
+    int first_line=slave_info->first_line;
+    int last_line=slave_info->last_line;
     int iter=slave_info->n_iter;
     int col=slave_info->colunas;
     int tarefas=slave_info->n_tarefas;
-    int linhas=col/tarefas;
-    double up=slave_info->cima;
-    double down=slave_info->baixo;
-    double left=slave_info->esq;
-    double right=slave_info->dir;
-    double recebe_linha[col+2];
-    matrix=dm2dNew(linhas+2,col+2);
-    matrix_aux=dm2dNew(linhas+2,col+2);
+    double maxD=slave_info->maxD;
+   
+    double diference;
+    int k,i,j;
+    double value;
     
-    //poe as temperaturas iniciais nas bordas das matrizes
-    dm2dSetColumnTo (matrix, 0, left);
-    dm2dSetColumnTo (matrix, col+1, right);
-    dm2dSetColumnTo (matrix_aux, 0, left);
-    dm2dSetColumnTo (matrix_aux, col+1, right);
-    
-    if (id==0){
-        dm2dSetLineTo (matrix, 0, up);
-        dm2dSetLineTo (matrix_aux, 0, up);
-    }
-    if (id==tarefas-1){
-        dm2dSetLineTo (matrix, linhas+1, down);
-        dm2dSetLineTo (matrix_aux, linhas+1, down);
-    }
-	
    	//atualiza matrix
-   	for (k=0;k<iter;k++){ 
-        for (i = 1; i < linhas+1; i++){
+   	for (k=0;k<iter;k++){
+        
+        if(pthread_mutex_lock(&mutex) != 0) {
+            fprintf(stderr, "\nErro ao bloquear mutex\n");
+            exit(-1);
+        }
+
+        while(saiu!=0){
+            if(pthread_cond_wait(&wait_to_wait,&mutex) != 0) {
+                fprintf(stderr, "\nErro ao esperar pela variável de condição\n");
+                exit(-1);
+            }
+        }
+        if(pthread_mutex_unlock(&mutex) != 0) {
+            fprintf(stderr, "\nErro ao desbloquear mutex\n");
+            exit(-1);
+        }
+        
+        for (i = first_line; i <= last_line; i++){
             for (j = 1; j < col+1; j++) {
                 value = (dm2dGetEntry(matrix, i-1, j) + dm2dGetEntry(matrix, i+1, j) + dm2dGetEntry(matrix, i, j-1) + dm2dGetEntry(matrix, i, j+1) ) / 4.0;
                 dm2dSetEntry(matrix_aux, i, j, value);
+                diference=value-dm2dGetEntry(matrix,i,j);
+                               
+                if(pthread_mutex_lock(&max_mutex) != 0) {
+                    fprintf(stderr, "\nErro ao bloquear mutex\n");
+                    exit(-1);
+                }
+                if (diference>max){
+                    max=diference;
+                }
+                if(pthread_mutex_unlock(&max_mutex) != 0) {
+                    fprintf(stderr, "\nErro ao desbloquear mutex\n");
+                    exit(-1);
+                }
             }
         }
-		
-		// se id for par, recebe e depois envia uma linha
-        if (id%2==0){
-            if (id!=0){ // se nao e a primeira thread, troca mensagens com a thread anterior (e atualiza a respetiva linha)
-                receberMensagem(id-1,id,recebe_linha, sizeof(double)*(col+2));
-                dm2dSetLine(matrix_aux,0,recebe_linha);
-                enviarMensagem(id,id-1,dm2dGetLine(matrix_aux,1), sizeof(double)*(col+2));
-                
-            }
-            if (id!=tarefas-1){ // se nao e a ultima thread, troca com a thread seguinte
-                receberMensagem(id+1,id,recebe_linha, sizeof(double)*(col+2)); 
-                dm2dSetLine(matrix_aux,linhas+1,recebe_linha);
-                enviarMensagem(id,id+1,dm2dGetLine(matrix_aux,linhas), sizeof(double)*(col+2));
+
+        if(pthread_mutex_lock(&mutex) != 0) {
+            fprintf(stderr, "\nErro ao bloquear mutex\n");
+            exit(-1);
+        }      
+       
+        tem_esperar++;
+        
+        while (tem_esperar!=tarefas){
+            if(pthread_cond_wait(&espera_por_todos,&mutex) != 0) {
+                fprintf(stderr, "\nErro ao esperar pela variável de condição\n");
+                exit(-1);
             }
         }
+        
+        saiu++;
+        
+        if (saiu==tarefas){
             
-        //se id for impar, envia e depois recebe uma linha
-        if (id%2==1){         
-            //se impar, nunca e a primeira thread
-            enviarMensagem(id,id-1,dm2dGetLine(matrix_aux,1), sizeof(double)*(col+2));
-            receberMensagem(id-1,id,recebe_linha, sizeof(double)*(col+2));
-            dm2dSetLine(matrix_aux,0,recebe_linha);
+            tmp = matrix_aux;
+            matrix_aux = matrix;
+            matrix = tmp;
             
-            if (id!=tarefas-1){
-                enviarMensagem(id,id+1,dm2dGetLine(matrix_aux,linhas), sizeof(double)*(col+2));
-                receberMensagem(id+1,id,recebe_linha,sizeof(double)*(col+2)); 
-                dm2dSetLine(matrix_aux,linhas+1,recebe_linha);
+            saiu=0;
+            tem_esperar=0;
+        }
+        
+        if (max<maxD){
+            if(pthread_cond_broadcast(&espera_por_todos) != 0) {
+                fprintf(stderr, "\nErro ao desbloquear variável de condição\n");
+                exit(-1);
+            }
+            if(pthread_mutex_unlock(&mutex) != 0) {
+                fprintf(stderr, "\nErro ao desbloquear mutex\n");
+                exit(-1);
+            }
+            return 0;
+        }
+        
+        if (saiu==0){
+            max=0;
+            if(pthread_cond_broadcast(&wait_to_wait) != 0) {
+            fprintf(stderr, "\nErro ao desbloquear variável de condição\n");
+            exit(-1);
             }
         }
-		
-        tmp = matrix_aux;
-        matrix_aux = matrix;
-        matrix = tmp;
-    }
-    //Saida, envia para a main
-    for (i=1;i<linhas+1;i++){
-        enviarMensagem(id,tarefas,dm2dGetLine(matrix,i),sizeof(double)*(col+2));
+        
+        if(pthread_cond_broadcast(&espera_por_todos) != 0) {
+            fprintf(stderr, "\nErro ao desbloquear variável de condição\n");
+            exit(-1);
+        }
+
+        if(pthread_mutex_unlock(&mutex) != 0) {           
+            fprintf(stderr, "\nErro ao desbloquear mutex\n");
+            exit(-1);
+        }
+             
     }
     return 0;
 }
@@ -157,7 +193,7 @@ int main (int argc, char** argv) {
 
     if(argc != 9) {
         fprintf(stderr, "\nNumero invalido de argumentos.\n");
-        fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes\n\n");
+        fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes trab maxD\n\n");
         return 1;
     }
 
@@ -169,65 +205,102 @@ int main (int argc, char** argv) {
     double tInf = parse_double_or_exit(argv[5], "tInf");
     int iteracoes = parse_integer_or_exit(argv[6], "iteracoes");
     int trab = parse_integer_or_exit(argv[7], "trab");
-    int csz = parse_integer_or_exit(argv[8], "csz");
+    double maxD = parse_double_or_exit(argv[8], "maxD");
 
     fprintf(stderr, "\nArgumentos:\n"
-	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d\n",
-	N, tEsq, tSup, tDir, tInf, iteracoes);
+	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d trab=%d maxD=%.1f\n",
+	N, tEsq, tSup, tDir, tInf, iteracoes, trab, maxD);
 
-    if(N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1 || N%trab!=0 || csz<0 || trab<0) {
+    if(N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1 || N%trab!=0 || maxD<0 || trab<0) {
         fprintf(stderr, "\nErro: Argumentos invalidos.\n"
-        " Lembrar que N >= 1, temperaturas >= 0 e iteracoes >= 1\n\n");
+        " Lembrar que N >= 1, temperaturas >= 0, iteracoes >= 1, N is divisble by trab, trab>0 e maxD>=0\n\n");
     return 1;
     }
     
-    DoubleMatrix2D *matrix_final; //matriz resultado, vai receber resultados das threads
-    matrix_final=dm2dNew(N+2,N+2);
-    dm2dSetLineTo(matrix_final,0,tSup);
-    dm2dSetLineTo(matrix_final,N+1,tInf);
-
+    //inicializacao matrix
+    matrix=dm2dNew(N+2,N+2);
+    matrix_aux=dm2dNew(N+2,N+2);    
     
-    info_t info[trab]; //array de estruturas com a informacao a passar a cada thread
+    dm2dSetLineTo(matrix,0,tSup);
+    dm2dSetLineTo(matrix_aux,0,tSup);    
+    dm2dSetLineTo(matrix,N+1,tInf);
+    dm2dSetLineTo(matrix_aux,N+1,tInf);
+    dm2dSetColumnTo (matrix, 0, tEsq);
+    dm2dSetColumnTo (matrix_aux, 0, tEsq);
+    dm2dSetColumnTo (matrix, N+1, tDir);
+    dm2dSetColumnTo (matrix_aux, N+1, tDir);
+    
+    if(pthread_mutex_init(&mutex, NULL) != 0) {
+        fprintf(stderr, "\nErro ao inicializar mutex\n");
+        return -1;
+    }
+    
+    if(pthread_mutex_init(&max_mutex, NULL) != 0) {
+        fprintf(stderr, "\nErro ao inicializar mutex\n");
+        return -1;
+    }
+  
+    if(pthread_cond_init(&espera_por_todos, NULL) != 0) {
+        fprintf(stderr, "\nErro ao inicializar variável de condição\n");
+        return -1;
+    }
+    if(pthread_cond_init(&wait_to_wait, NULL) != 0) {
+        fprintf(stderr, "\nErro ao inicializar variável de condição\n");
+        return -1;
+    }
+    info_t *info;
+    info=(info_t*)malloc(trab*sizeof(info_t)); //array de estruturas com a informacao a passar a cada thread
+
     pthread_t *threads;
     threads=(pthread_t*)malloc(trab*sizeof(pthread_t)); //array com id de cada thread
-    
-    if (inicializarMPlib(csz, trab+1)==-1){
-        printf("Erro ao inicializarMPlib.\n"); return 1;
-    }
-     
+ 
     int i;
+        
     for (i=0;i<trab;i++){
         //atualiza info para cada thread
-        info[i].t_id=i;
+        info[i].maxD=maxD;
         info[i].n_iter=iteracoes;
+        info[i].first_line=i*N/trab+1;
+        info[i].last_line=(i+1)*N/trab;        
         info[i].colunas=N;
         info[i].n_tarefas=trab;
-        info[i].cima=tSup;
-        info[i].baixo=tInf;
-        info[i].esq=tEsq;
-        info[i].dir=tDir;
-    
         pthread_create(&threads[i],NULL,simul, &info[i]);
     }
     
     //Saida
-    double recebe_linha[N+2];
-    int k;
-    //para cada thread (i) copia cada linha(k) para a matriz principal, e acaba a thread
     for (i=0;i<trab;i++){
-            for (k=1;k<(N/trab+1);k++){
-                receberMensagem(i,trab,recebe_linha,sizeof(double)*(N+2));
-                dm2dSetLine(matrix_final,N/trab*i+k,recebe_linha);
-        }
         if (pthread_join(threads[i],NULL)){
             fprintf(stderr, "\nErro ao esperar por um escravo\n");
             return -1;
         }
     }  
         
-  dm2dPrint(matrix_final);
-  libertarMPlib();
-  dm2dFree(matrix_final);
+   dm2dPrint(matrix);
+   
+   if(pthread_mutex_destroy(&mutex) != 0) {
+        fprintf(stderr, "\nErro ao destruir mutex\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if(pthread_mutex_destroy(&max_mutex) != 0) {
+        fprintf(stderr, "\nErro ao destruir mutex\n");
+        exit(EXIT_FAILURE);
+    }
+  
+    if(pthread_cond_destroy(&espera_por_todos) != 0) {
+        fprintf(stderr, "\nErro ao destruir variável de condição\n");
+        exit(EXIT_FAILURE);
+    }
+    if(pthread_cond_destroy(&wait_to_wait) != 0) {
+        fprintf(stderr, "\nErro ao destruir variável de condição\n");
+        exit(EXIT_FAILURE);
+    }
+  
+   dm2dFree(matrix);
+   dm2dFree(matrix_aux);
+   free(threads);
+   free(info);
+  
   
   return 0;
 }
